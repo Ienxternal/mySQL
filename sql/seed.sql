365e22b4c2abead1f405cfcf29874ca0224db951
Ienxternal <alexanderttnguyen@gmail.com>
2023-08-14 17:33:53 -0500

    Add files

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,40 +1,40 @@
-USE employeesDB;
+USE companyDB;
 
 INSERT INTO department (name)
-VALUES ("Sales");
+VALUES ("Marketing");
 INSERT INTO department (name)
-VALUES ("Engineering");
+VALUES ("Research");
 INSERT INTO department (name)
-VALUES ("Finance");
+VALUES ("Accounting");
 INSERT INTO department (name)
-VALUES ("Legal");
+VALUES ("Legal Affairs");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 100000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 150000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 125000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 250000, 4);
+INSERT INTO position (title, salary, department_id)
+VALUES ("Marketing Manager", 95000, 1);
+INSERT INTO position (title, salary, department_id)
+VALUES ("Research Director", 145000, 2);
+INSERT INTO position (title, salary, department_id)
+VALUES ("Research Associate", 110000, 2);
+INSERT INTO position (title, salary, department_id)
+VALUES ("Accountant", 115000, 3);
+INSERT INTO position (title, salary, department_id)
+VALUES ("Legal Director", 230000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jane", "Doe", 1, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Norah", "Rios", 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kate", "Mellor", 3, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Shiv", "Betts", 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Leen", "Hurst", 5, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Gary", "Leer", 2, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Cruise", 4, 7);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Daisy", "Hooper", 1, 2);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Alice", "Smith", 1, 3);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Ella", "Robinson", 2, 1);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Grace", "Taylor", 3, null);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Oliver", "Baker", 4, 3);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Sophie", "Wood", 5, null);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Henry", "Walker", 2, null);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Daniel", "Evans", 4, 7);
+INSERT INTO employee (first_name, last_name, position_id, manager_id)
+VALUES ("Mia", "Turner", 1, 2);
 
 SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
